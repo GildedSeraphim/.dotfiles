5fdf436cdbca7fc6d13685575224af6a895e24c8
GildedSeraphim <nathanishaaz@gmail.com>
2024-12-11 11:13:46 -0600

    dressing

--- hm/programs/nixvim/plugins/ui/default.nix
+++ hm/programs/nixvim/plugins/ui/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
     ./dashboard.nix
+    ./dressing.nix
   ];
 }
---
+++ hm/programs/nixvim/plugins/ui/dressing.nix
@@ -0,0 +1,42 @@
+{...}: {
+  programs.nixvim = {
+    plugins = {
+      dressing = {
+        settings = {
+          input = {
+            enabled = true;
+            mappings = {
+              i = {
+                "<C-c>" = "Close";
+                "<CR>" = "Confirm";
+                "<Down>" = "HistoryNext";
+                "<Up>" = "HistoryPrev";
+              };
+              n = {
+                "<CR>" = "Confirm";
+                "<Esc>" = "Close";
+              };
+            };
+          };
+          select = {
+            backend = [
+              "telescope"
+              "fzf_lua"
+              "fzf"
+              "builtin"
+              "nui"
+            ];
+            builtin = {
+              mappings = {
+                "<C-c>" = "Close";
+                "<CR>" = "Confirm";
+                "<Esc>" = "Close";
+              };
+            };
+            enabled = true;
+          };
+        };
+      };
+    };
+  };
+}
