5524fa796fba7660555fbd59ff92caa5990575aa
GildedSeraphim <nathanishaaz@gmail.com>
2024-12-11 12:45:48 -0600

    finshed nvim conf

--- hm/programs/nixvim/default.nix
+++ hm/programs/nixvim/default.nix
@@ -16,5 +16,34 @@
         enable = true;
       };
     };
+    opts = {
+      relativenumber = true;
+      number = true;
+
+      tabstop = 4;
+      shiftwidth = 4;
+      expandtab = true;
+      autoindent = true;
+
+      wrap = false;
+
+      ignorecase = true;
+      smartcase = true;
+
+      cursorline = true;
+
+      termguicolors = true;
+      background = "dark";
+      signcolumn = "yes";
+
+      backspace = "indent,eol,start";
+
+      splitright = true;
+      splitbelow = true;
+
+      swapfile = false;
+
+      laststatus = 3;
+    };
   };
 }
