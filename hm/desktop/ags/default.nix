1b36765f005ee45fae3cb6706e1b51badd52a505
GildedSeraphim <nathanishaaz@gmail.com>
2024-12-14 12:08:28 -0600

    ags install

--- hm/desktop/ags/default.nix
+++ hm/desktop/ags/default.nix
@@ -1,4 +1,18 @@
-{...}:
 {
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.ags.homeManagerModules.default
+  ];
 
+  programs.ags = {
+    enable = true;
+    configDir = ./ags;
+
+    extraPackages = with pkgs; [
+      inputs.ags.packages.${pkgs.system}.battery
+    ];
+  };
 }
