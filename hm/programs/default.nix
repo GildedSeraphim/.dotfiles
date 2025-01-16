20b4a1b998eae01f98aaef417cb12945560cb7e4
GildedSeraphim <nathanishaaz@gmail.com>
2025-01-16 16:42:15 -0600

    foo terminal

--- hm/programs/default.nix
+++ hm/programs/default.nix
@@ -16,5 +16,6 @@
     #      ./firefox
     ./glava
     ./nixvim
+    ./foot
   ];
 }
---
+++ hm/programs/foot/default.nix
@@ -0,0 +1,11 @@
+{}: {
+  programs.foot = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      mouse = {
+        hide_when_typing = "yes";
+      };
+    };
+  };
+}
