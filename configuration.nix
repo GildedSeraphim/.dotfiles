e822aad8c71de6baa6404e07e6eab6f4fd0fe63a
GildedSeraphim <snathani@protonmail.com>
2025-02-17 11:15:05 -0600

    huh?

--- configuration.nix
+++ configuration.nix
@@ -8,8 +8,8 @@
     #./hardware/laptop/hardware-configuration.nix
     #./hardware/laptop/nvidia.nix
     #./hardware/laptop/laptop.nix
-    ./hardware/desktop/nvidia.nix
-    ./hardware/desktop/hardware-configuration.nix
+    #./hardware/desktop/nvidia.nix
+    #./hardware/desktop/hardware-configuration.nix
     ./settings.nix
     ./fonts.nix
     ./hyprland.nix
@@ -18,7 +18,7 @@
     #./hardware/rog-strix/nvidia.nix
     #./hardware/rog-strix/hardware-configuration.nix
     ./hardware/keyboard
-    #./hardware/proart
+    ./hardware/proart
   ];
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
@@ -66,7 +66,7 @@
       # ollama-cuda
     ]);
   services.open-webui = {
-    enable = true;
+    enable = false;
     #localhost/8080
   };
 }
