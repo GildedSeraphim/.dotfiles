a63707563b5c9798ff5c670388010f8858930e60
GildedSeraphim <snathani@protonmail.com>
2025-02-18 19:10:57 -0600

    cachix binary for hyprland

--- settings.nix
+++ settings.nix
@@ -46,7 +46,10 @@
   services.xserver.enable = true;
 
   nix.settings.auto-optimise-store = true;
-
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
   environment.sessionVariables = {
     FLAKE = "/home/sn/.dotfiles";
     NIXOS_CONFIG = "/home/sn/.dotfiles/";
