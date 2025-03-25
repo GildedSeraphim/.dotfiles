f63b75824e4c0547a44ef1fbfbbdee06422dde7e
GildedSeraphim <nathanishaaz@gmail.com>
2025-03-25 11:57:09 -0500

    back to zen

--- hm/desktop/xdg/default.nix
+++ hm/desktop/xdg/default.nix
@@ -5,7 +5,7 @@
     mimeApps = {
       enable = true;
       defaultApplications = let
-        browser = ["librewolf.desktop"];
+        browser = ["zen.desktop"];
         fileManager = ["org.gnome.Nautilus.desktop"];
         editor = ["vscodium.desktop"];
         player = ["vlc.desktop"];
