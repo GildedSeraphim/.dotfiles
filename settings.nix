0f82271d84454b3be42034817d64f00a78eaef60
GildedSeraphim <snathani@protonmail.com>
2025-06-10 10:19:48 -0500

    update flake

--- settings.nix
+++ settings.nix
@@ -13,7 +13,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 1w";
+    options = "--delete-older-than 14d";
   };
 
   security.rtkit.enable = true;
