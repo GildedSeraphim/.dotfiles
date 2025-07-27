3a5432c1b667445b010ed6038204034e1b37c988
GildedSeraphim <nathanishaaz@gmail.com>
2025-07-26 23:50:54 -0500

    wallpaper

--- stylix.nix
+++ stylix.nix
@@ -7,7 +7,7 @@
   theme = "ayu-dark";
   th = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
   #alterfile = ./wal/color.txt;
-  wallpaper = ./wal/ship.png;
+  wallpaper = ./wal/brick.png;
   #altered-wallpaper = pkgs.runCommand "altered.png" {} ''
   #${pkgs.imagemagick}/bin/magick convert ${wallpaper} -colorspace sRGB -color-matrix < ${alterfile} altered.png $out
   #'';
