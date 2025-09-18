57cd0b3e1cedcc13eacc820a1065eec5da6a99e8
GildedSeraphim <nathanishaaz@gmail.com>
2025-09-18 17:24:43 -0500

    more network

--- network.nix
+++ network.nix
@@ -75,7 +75,7 @@
       wg0 = {
         autostart = false;
         address = [ "10.2.0.2/32" ];
-        dns = [ "10.2.0.1" ];
+        #dns = [ "10.2.0.1" ];
         privateKeyFile = "/home/sn/keys/proton.key";
         peers = [
           {
